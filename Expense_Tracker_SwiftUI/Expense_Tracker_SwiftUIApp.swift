282f1bb50dee4ea23d0bd206390c0cb59973aff4
MehdiALOMER <mehdi.alomer98@gmail.com>
2023-12-11 09:17:01 +0300

    Initial Commit

---
+++ Expense_Tracker_SwiftUI/Expense_Tracker_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_Tracker_SwiftUIApp.swift
+//  Expense_Tracker_SwiftUI
+//
+//  Created by Mehdi on 11.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Expense_Tracker_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
